53bd4adfd5e0197c65480482c387c01ab75e9e92
Cyprien G <github@guillemot.me>
2024-11-20 17:51:11 +0100

    Add shortcuts to easily navigate between buffers and windows

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,3 +12,13 @@ vim.keymap.set("n", "<leader>gH", "<cmd>Telescope git_bcommits<cr>", { desc = "O
 
 -- Exit terminal insert mode
 vim.keymap.set("t", "<C-Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
+
+-- Window navigation
+vim.keymap.set("n", "<A-Right>", "<C-w><Right>", { desc = "Go to right window", remap = true })
+vim.keymap.set("n", "<A-Left>", "<C-w><Left>", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<A-Up>", "<C-w><Up>", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<A-Down>", "<C-w><Down>", { desc = "Go to lower window", remap = true })
+
+-- Buffer navigation
+vim.keymap.set("n", "<A-S-Right>", "<cmd>bnext<cr>", { desc = "Next buffer", remap = true })
+vim.keymap.set("n", "<A-S-Left>", "<cmd>bprevious<cr>", { desc = "Previous buffer", remap = true })
