ac08828363f621342d9940e5d3e68381bcff1b1e
Cyprien G <github@guillemot.me>
2024-11-20 17:51:10 +0100

    Lock terminal buffers

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Create an autocommand group
+vim.api.nvim_create_augroup("TerminalFix", { clear = true })
+
+-- Set winfixbuf for terminal buffers
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = "TerminalFix",
+  pattern = "*",
+  command = "setlocal winfixbuf",
+})
