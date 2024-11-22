afb1030f13e0f63d16271fc64fa9fb238d1b2fdc
Cyprien G <github@guillemot.me>
2024-11-22 17:34:40 +0100

    Change autocomplete keymaps

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -35,4 +35,18 @@ return {
       },
     },
   },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = cmp.mapping({
+        ["<tab>"] = cmp.mapping.confirm({ select = true }),
+        ["<esc>"] = cmp.mapping.abort(),
+        ["<Down>"] = cmp.mapping.select_next_item(),
+        ["<Up>"] = cmp.mapping.select_prev_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      })
+    end,
+  },
 }
