13e65a49f91697d6b12fe473be1b64f8633db1bf
Cyprien G <github@guillemot.me>
2024-11-22 17:23:18 +0100

    Neotree: put in system trash

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -4,6 +4,14 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       filesystem = {
+        commands = {
+          -- Override delete to use trash instead of rm
+          delete = function(state)
+            local path = state.tree:get_node().path
+            vim.fn.system({ "trash", vim.fn.fnameescape(path) })
+            require("neo-tree.sources.manager").refresh(state.name)
+          end,
+        },
         filtered_items = {
           show_hidden_count = true,
           hide_dotfiles = false,
