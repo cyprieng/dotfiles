a5b35e8793ae98a6619d954c6cdc0e6270698719
Cyprien Guillemot <github@guillemot.me>
2024-12-05 12:31:54 +0100

    refactor(bettertouchtool): move script in bettertouchtool directory

--- backup_bettertouchtool.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Current directory
-BASEDIR=$(realpath "$(dirname "$0")")
-
-cp ~/Library/Preferences/com.hegenberg.BetterTouchTool.plist "$BASEDIR/bettertouchtool/com.hegenberg.BetterTouchTool.plist"
