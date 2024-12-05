a4123cbd0a497f330bf32c32d09f11f60bb36c0a
Cyprien Guillemot <github@guillemot.me>
2024-12-05 08:14:41 +0100

    feat(bettertouchtool): use symlink instead of preset import

--- install.sh
+++ install.sh
@@ -58,11 +58,13 @@ link_file "$BASEDIR/ranger" ~/.config/ranger
 link_file "$BASEDIR/vale" ~/.config/vale
 link_file "$BASEDIR/aider/.aider.conf.yml" ~/.aider.conf.yml
 link_file "$BASEDIR/lazygit/config.yml" ~/Library/Application\ Support/lazygit/config.yml
-link_file "$BASEDIR/bettertouchtool" ~/.config/bettertouchtool
 link_file "$BASEDIR/sqlfluff/.sqlfluff" ~/.sqlfluff
 link_file "$BASEDIR/karabiner" ~/.config/karabiner
 link_file "$BASEDIR/hammerspoon" ~/.hammerspoon
 link_file "$BASEDIR/commitizen/.cz.toml" ~/.cz.toml
+
+link_file "$BASEDIR/bettertouchtool/library" ~/Library/Application\ Support/BetterTouchTool
+link_file "$BASEDIR/bettertouchtool/com.hegenberg.BetterTouchTool.plist" ~/Library/Preferences/com.hegenberg.BetterTouchTool.plist
 
 # Git config
 link_file "$BASEDIR/git/.gitconfig" ~/.gitconfig-global
@@ -77,9 +79,6 @@ fi
 
 # Weather
 go install git.sr.ht/~timharek/yr@latest
-
-# Bettertouchtool
-open ~/.config/bettertouchtool/default.bttpreset
 
 # Alt tab
 defaults import com.lwouis.alt-tab-macos "$BASEDIR/alttab/com.lwouis.alt-tab-macos.plist"
