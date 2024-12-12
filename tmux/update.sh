99bcc3646f5e3da5e1450f7a6ad63c5bf33f62c3
Cyprien Guillemot <github@guillemot.me>
2024-12-12 08:27:35 +0100

    fix(tmux): fix tpm with tmux installed with homebrew

--- tmux/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-# Current directory
-BASEDIR=$(realpath "$(dirname "$0")")
-
-wget -O "$BASEDIR/tmux.conf" https://raw.githubusercontent.com/gpakosz/.tmux/refs/heads/master/.tmux.conf
