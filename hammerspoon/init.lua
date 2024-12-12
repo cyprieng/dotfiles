dda119bf2d262d4af8c3e67011f4cd1ec57a7b77
Cyprien G <github@guillemot.me>
2024-12-12 14:27:00 +0100

    feat(hammerspoon): auto switch keyboard layout when physical keyboard is plugged in

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,2 +1,3 @@
 require("raycast")
 require("tile")
+require("keyboard")
---
+++ hammerspoon/keyboard.lua
@@ -0,0 +1,48 @@
+-- Keep track of USB keyboard state
+local usbKeyboardConnected = false
+
+-- Input source IDs
+local FRENCH_INPUT = "com.apple.keylayout.French"
+local US_INTL_INPUT = "com.apple.keylayout.USInternational-PC"
+
+-- Function to change input source
+function changeInputSource(sourceID)
+	hs.keycodes.currentSourceID(sourceID)
+end
+
+-- Function to check USB keyboard status and change input source
+function handleKeyboardConnection()
+	local devices = hs.usb.attachedDevices()
+	local keyboardFound = false
+
+	-- Check for USB keyboards
+	for _, device in pairs(devices) do
+		-- You might need to adjust this check based on your keyboard's properties
+		if device.productName and device.productName:lower():match("xd87") then
+			keyboardFound = true
+			break
+		end
+	end
+
+	-- If keyboard state changed
+	if keyboardFound ~= usbKeyboardConnected then
+		usbKeyboardConnected = keyboardFound
+
+		if keyboardFound then
+			-- When USB keyboard is connected -> US International
+			changeInputSource(US_INTL_INPUT)
+			hs.alert.show("Switched to US International")
+		else
+			-- When USB keyboard is disconnected -> French
+			changeInputSource(FRENCH_INPUT)
+			hs.alert.show("Switched to French")
+		end
+	end
+end
+
+-- Watch for USB device changes
+usbWatcher = hs.usb.watcher.new(handleKeyboardConnection)
+usbWatcher:start()
+
+-- Initial check
+handleKeyboardConnection()
