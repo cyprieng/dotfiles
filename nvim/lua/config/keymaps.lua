7a36b2c07ed73c6305bc7788c906f419b18917cd
Cyprien Guillemot <github@guillemot.me>
2024-12-12 14:07:29 +0100

    feat(nvim): update keymaps for word and window navigation

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -14,11 +14,19 @@ vim.keymap.set("n", "<leader>gH", "<cmd>Telescope git_bcommits<cr>", { desc = "O
 vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
 
 -- Window navigation
-vim.keymap.set("n", "<A-Right>", "<C-w><Right>", { desc = "Go to right window", remap = true })
-vim.keymap.set("n", "<A-Left>", "<C-w><Left>", { desc = "Go to left window", remap = true })
-vim.keymap.set("n", "<A-Up>", "<C-w><Up>", { desc = "Go to upper window", remap = true })
-vim.keymap.set("n", "<A-Down>", "<C-w><Down>", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<leader><Right>", "<C-w><Right>", { desc = "Go to right window", remap = true })
+vim.keymap.set("n", "<leader><Left>", "<C-w><Left>", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<leader><Up>", "<C-w><Up>", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<leader><Down>", "<C-w><Down>", { desc = "Go to lower window", remap = true })
 
 -- Buffer navigation
 vim.keymap.set("n", "<A-S-Right>", "<cmd>bnext<cr>", { desc = "Next buffer", remap = true })
 vim.keymap.set("n", "<A-S-Left>", "<cmd>bprevious<cr>", { desc = "Previous buffer", remap = true })
+
+-- Word navigation
+vim.keymap.set("n", "<A-Right>", "w", { noremap = true })
+vim.keymap.set("n", "<A-Left>", "b", { noremap = true })
+vim.keymap.set("i", "<A-Right>", "<C-o>w", { noremap = true })
+vim.keymap.set("i", "<A-Left>", "<C-o>b", { noremap = true })
+vim.keymap.set("v", "<A-Right>", "w", { noremap = true })
+vim.keymap.set("v", "<A-Left>", "b", { noremap = true })
