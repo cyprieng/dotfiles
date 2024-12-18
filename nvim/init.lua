17def725f6889e0b6a7cab81813e6ca1f314308b
Cyprien G <github@guillemot.me>
2024-12-18 16:48:16 +0100

    feat(neovim): autoreload files

--- nvim/init.lua
+++ nvim/init.lua
@@ -102,6 +102,15 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd("checktime")
+    end
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
