9b4b5fddb695049a737cfde36d70c430f44501d8
Cyprien G <github@guillemot.me>
2024-12-27 11:56:35 +0100

    feat(neovim): enter automatically in insertmode when entering in a term buffer

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -17,3 +17,14 @@ vim.api.nvim_create_autocmd({ "InsertLeave" }, {
     end
   end,
 })
+
+-- Auto insert mode when entering in a term buffer
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = "term://*", -- Correspond à tous les buffers de terminal
+  group = vim.api.nvim_create_augroup("terminal_insert", { clear = true }),
+  callback = function()
+    vim.schedule(function()
+      vim.cmd("startinsert")
+    end)
+  end,
+})
