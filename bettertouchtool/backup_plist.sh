dd28205c3bc8794e20e5e5c713b4d16eb1ff0e69
Cyprien G <github@guillemot.me>
2024-12-30 14:24:33 +0100

    fix(bettertouchtool): avoid keeping all config versions

--- bettertouchtool/backup_plist.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Current directory
-BASEDIR=$(realpath "$(dirname "$0")")
-
-cp ~/Library/Preferences/com.hegenberg.BetterTouchTool.plist "$BASEDIR/com.hegenberg.BetterTouchTool.plist"
