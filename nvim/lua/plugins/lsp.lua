517cf9a0e1dadbb4650c387e0185a4892e6c0091
Cyprien G <github@guillemot.me>
2025-01-02 17:49:10 +0100

    feat(neovim): show ghost text in nvim-cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -437,18 +437,12 @@ return {
       local luasnip = require("luasnip")
       luasnip.config.setup({})
 
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+
       -- Tailwind colors
       require("tailwindcss-colorizer-cmp").setup({
         color_square_width = 2,
       })
-
-      local has_words_before = function()
-        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-          return false
-        end
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-      end
 
       cmp.setup({
         formatting = {
@@ -505,6 +499,9 @@ return {
             cmp.config.compare.length,
             cmp.config.compare.order,
           },
+        },
+        experimental = {
+          ghost_text = { hl_group = "CmpGhostText" },
         },
       })
     end,
