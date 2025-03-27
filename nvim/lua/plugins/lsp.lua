34f148d24a664280ab29cba2d0ee9330a80b87dd
Cyprien G <github@guillemot.me>
2025-03-27 17:39:55 +0100

    feat(neovim): improve diagnostic display

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -137,14 +137,26 @@ return {
       })
 
       -- Change diagnostic symbols in the sign column (gutter)
-      -- if vim.g.have_nerd_font then
-      --   local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
-      --   local diagnostic_signs = {}
-      --   for type, icon in pairs(signs) do
-      --     diagnostic_signs[vim.diagnostic.severity[type]] = icon
-      --   end
-      --   vim.diagnostic.config { signs = { text = diagnostic_signs } }
-      -- end
+      vim.diagnostic.config({
+        virtual_text = true, -- Show diagnostics as virtual text
+        signs = true, -- Show signs in the sign column
+        underline = true, -- Use underline for diagnostics
+        update_in_insert = false, -- Don't update diagnostics in insert mode
+        severity_sort = true, -- Sort diagnostics by severity
+        float = {
+          show_header = true,
+          border = "rounded",
+        },
+      })
+
+      if vim.g.have_nerd_font then
+        local signs = { ERROR = "", WARN = "", INFO = "", HINT = "" }
+        local diagnostic_signs = {}
+        for type, icon in pairs(signs) do
+          diagnostic_signs[vim.diagnostic.severity[type]] = icon
+        end
+        vim.diagnostic.config({ signs = { text = diagnostic_signs } })
+      end
 
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
