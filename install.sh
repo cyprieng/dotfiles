f70633bc41bcbfafadaf7b0b94180e007626070e
Cyprien Guillemot <github@guillemot.me>
2025-03-28 08:41:02 +0100

    feat: install java

--- install.sh
+++ install.sh
@@ -205,6 +205,7 @@ asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add rust https://github.com/asdf-community/asdf-rust.git
 asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
 asdf plugin add python https://github.com/asdf-community/asdf-python.git
+asdf plugin add java https://github.com/halcyon/asdf-java.git
 asdf plugin update --all
 asdf install
 
