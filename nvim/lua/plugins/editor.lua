32fe07cb07ee847cd721296c4019dc9764f1f4fe
Cyprien G <github@guillemot.me>
2025-03-31 16:17:03 +0200

    feat(neovim): add auto split/join

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -201,4 +201,14 @@ return {
       },
     },
   },
+
+  -- Split/join
+  {
+    "Wansmer/treesj",
+    keys = { "<space>m", "<space>j", "<space>s" },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesj").setup({})
+    end,
+  },
 }
