93eaefe672c4469b3ec4b8399c79b65dbab8995c
Cyprien G <github@guillemot.me>
2025-04-07 17:43:04 +0200

    feat(neovim): prevent dd from copying

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,10 @@ local map = vim.keymap.set
 
 local Snacks = require("snacks")
 
+-- Prevent deleting from also copying
+map({ "n", "v" }, "d", '"_d', { noremap = true })
+map("n", "dd", '"_dd', { noremap = true })
+
 -- Avante chat clear
 map("n", "<leader>al", "<cmd>AvanteClear<cr>", { desc = "Clear Avante chat" })
 
