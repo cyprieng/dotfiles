c144aad723599ec282d432019e70b5eb599fac82
Cyprien G <github@guillemot.me>
2025-04-17 09:08:20 +0200

    feat(finder): change defautl search scope to current folder

--- install.sh
+++ install.sh
@@ -149,6 +149,7 @@ if $macos; then
   defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true             # Disable .DS_Store on USB volumes
   defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"                  # Use list view in all Finder windows by default
   defaults write com.apple.finder NewWindowTarget -string "PfHm"                       # Set home as default finder location
+  defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"                  # Search the current folder by default
   defaults write com.apple.dock expose-group-by-app -bool false                        # Don’t group windows by application in Mission Control
   defaults write com.apple.dock autohide-delay -float 0                                # Remove the auto-hiding Dock delay
   defaults write com.apple.dock autohide-time-modifier -float 0                        # Remove the animation when hiding/showing the Dock
