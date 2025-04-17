3ec79a34705e4a84bbba88b946700035f4c8f8cf
Cyprien Guillemot <github@guillemot.me>
2025-04-17 08:15:52 +0200

    fix(install.sh): ensure PiPHero is running

--- install.sh
+++ install.sh
@@ -192,6 +192,7 @@ if $macos; then
   open_app_if_not_running "Stats"
   open_app_if_not_running "Dropshelf"
   open_app_if_not_running "CleanShot X"
+  open_app_if_not_running "PiPHero"
 
   # Use TouchId for sudo
   if ! grep -q "pam_tid.so" /etc/pam.d/sudo; then
