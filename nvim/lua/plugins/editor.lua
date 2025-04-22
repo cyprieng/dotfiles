f1071472d179a78cd30c68d9d17f6c9a21fa5174
Cyprien G <github@guillemot.me>
2025-04-22 14:59:24 +0200

    feat(neovim): update gitsigns symbols

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -74,13 +74,6 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     opts = {
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
       current_line_blame = true, -- Shows git blame for current line
       current_line_blame_opts = {
         virt_text = true,
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -142,16 +142,6 @@ return {
               modified = " ",
               removed = " ",
             },
-            source = function()
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
           },
         },
         lualine_y = {
