947aae0aa73af49cfef8373369a7147e8e9de01f
Cyprien G <github@guillemot.me>
2025-04-22 10:37:32 +0200

    feat(neovim): save buffer when leaving it

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -48,3 +48,13 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.cmd("silent Sleuth")
   end,
 })
+
+-- Save buffer when leaving
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+  pattern = "*",
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+      vim.api.nvim_command("silent update")
+    end
+  end,
+})
