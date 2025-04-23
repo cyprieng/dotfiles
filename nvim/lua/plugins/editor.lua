22746f831458ca8e94f0afd5ea22a391237ce618
Cyprien G <github@guillemot.me>
2025-04-23 09:56:20 +0200

    feat(neovim): use trouble instead of quickfix for telescope

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -144,8 +144,51 @@ return {
   -- Trouble
   {
     "folke/trouble.nvim",
+    opts = {
+      focus = true,
+      win = {
+        position = "bottom",
+        size = 0.3,
+      },
+      preview = {
+        type = "split",
+        relative = "win",
+        position = "right",
+        size = 0.3,
+      },
+    },
+    cmd = "Trouble",
     keys = {
-      { "<leader>cs", false },
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
     },
   },
 
