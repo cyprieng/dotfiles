377ea763b70fcf4349a6569e21c2746b98f5fb76
Cyprien G <github@guillemot.me>
2025-05-21 09:06:30 +0200

    feat(neovim): add treesitter context

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -602,4 +602,9 @@ return {
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+
+  -- Treesitter context
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+  },
 }
