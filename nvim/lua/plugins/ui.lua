64f03797e744844c6ca300898aa939d55fc6f97b
Cyprien G <github@guillemot.me>
2025-06-24 14:30:21 +0200

    feat(neovim): remove bufferline diagnostics indicator

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -353,6 +353,7 @@ return {
       { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
       { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
       { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+      { "<leader>bs", "<Cmd>BufferLinePick<CR>", desc = "Pick buffer" },
       { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
       { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
       { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
@@ -369,14 +370,8 @@ return {
         right_mouse_command = function(n)
           Snacks.bufdelete(n)
         end,
-        diagnostics = "nvim_lsp",
+        diagnostics = "none",
         always_show_bufferline = true,
-        diagnostics_indicator = function(_, _, diag)
-          local icons = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-            .. (diag.warning and icons.Warn .. diag.warning or "")
-          return vim.trim(ret)
-        end,
         offsets = {
           {
             filetype = "neo-tree",
