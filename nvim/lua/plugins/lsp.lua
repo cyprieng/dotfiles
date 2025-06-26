d5758cb3a1da4cfd3abfd008de285aa09781932e
Cyprien G <github@guillemot.me>
2025-06-26 15:26:13 +0200

    fix(neovim): avoid two ts_ls and typescript-tools running at the same time

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -292,12 +292,14 @@ return {
       vim.list_extend(ensure_installed, {
         "stylua", -- Used to format Lua code
         "prettier",
-        "ts_ls",
       })
+      local ensure_enabled = { unpack(ensure_installed) }
+      vim.list_extend(ensure_installed, { "ts_ls" })
+
       require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
       require("mason-lspconfig").setup({
-        automatic_enable = ensure_installed,
+        automatic_enable = ensure_enabled,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
