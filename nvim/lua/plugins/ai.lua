cc33727876be29cc6eeea870fea66de0cc2080c2
Cyprien G <github@guillemot.me>
2025-07-07 12:03:59 +0200

    feat(neovim): add mcp hub

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -27,6 +27,19 @@ return {
   },
 
   {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "bundled_build.lua",
+    config = function()
+      require("mcphub").setup({
+        use_bundled_binary = true,
+      })
+    end,
+  },
+
+  {
     "olimorris/codecompanion.nvim",
     opts = {
       strategies = {
@@ -53,6 +66,14 @@ return {
         vectorcode = {
           opts = {
             add_tool = true,
+          },
+        },
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            show_result_in_chat = true, -- Show mcp tool results in chat
+            make_vars = true, -- Convert resources to #variables
+            make_slash_commands = true, -- Add prompts as /slash commands
           },
         },
       },
