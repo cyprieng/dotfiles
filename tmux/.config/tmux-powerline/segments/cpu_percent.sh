153d75aaaa476162034964fa604fa7d3f8192685
Cyprien G <github@guillemot.me>
2025-10-28 09:13:26 +0100

    fix(tmux): add -e to top to get cpu usage

--- tmux/.config/tmux-powerline/segments/cpu_percent.sh
+++ tmux/.config/tmux-powerline/segments/cpu_percent.sh
@@ -2,5 +2,5 @@
 
 # Run segment
 run_segment() {
-  top -l 1 | grep "CPU usage" | awk -F'[:,%]' '{printf " %d%%\n", ($2+$4)}'
+  top -e -l 1 | grep "CPU usage" | awk -F'[:,%]' '{printf " %d%%\n", ($2+$4)}'
 }
