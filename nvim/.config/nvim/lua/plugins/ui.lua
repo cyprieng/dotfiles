4e86c6a783a1305e4544a5313596c739298f34f2
Cyprien Guillemot <cyprien@guillemot.me>
2026-03-14 15:01:12 +0100

    fix(neovim): lualine use auto theme

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -89,7 +89,7 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       options = {
-        theme = "catppuccin",
+        theme = "auto",
         globalstatus = true,
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
